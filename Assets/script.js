ec39834662a15d243a8ba503c47b590f3913ab80
Chris Messa <blindsun95@gmail.com>
2023-04-11 20:58:29 -0500

    added search function for city

--- Assets/script.js
+++ Assets/script.js
@@ -1,48 +1,52 @@
 const APIKey = "b4b306d022e640a29cb62888f869d314";
-const cityNameEl = document.querySelector('#city-name')
+const cityNameEl = document.querySelector('#city-name');
+const cityTempEl = document.querySelector('#current-temperature');
+const cityWindEl = document.querySelector('#current-wind');
+const cityHumidityEl = document.querySelector('#current-humidity');
+const currentIconEl = document.querySelector('#current-icon');
+const searchButton = document.querySelector('#search-btn');
+const searchInputEl = document.querySelector('#search-input');
 
-const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=chicago&appid=${APIKey}`;
-// const fiveDayURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon={lon}&appid=${APIKey}`;
-const geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=chicago&appid=${APIKey}`
-let latitude = ""
-let longitude = "";
-function getLat() {
-    return fetch(geoCodeURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        return data[0].lat;
-    });
-}
 
-function getLong() {
-    return fetch(geoCodeURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        return data[0].lon;
-    });
-}
-function getFiveDayURL() {
-    return Promise.all([getLat(), getLong()])
-      .then(function (values) {
-        latitude = values[0];
-        longitude = values[1];
-        const fiveDayURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}`;
-        fetch(fiveDayURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            cityNameEl.textContent = data.city.name
-        })
-      });
-    }
+searchButton.addEventListener('click', function () {
+    let searchedCity = searchInputEl.value.toLowerCase().trim();
+    
+    queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${APIKey}&units=imperial`;
 
-getFiveDayURL()
-
-$(function() {
-
+getCoords();
 });
+
+
+function getCoords() {
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        cityNameEl.textContent = `${data.name}`;
+        cityTempEl.textContent = `Temp: ${data.main.temp}`;
+        cityWindEl.textContent = `Wind: ${data.wind.speed} MPH`;
+        cityHumidityEl.textContent = `Humidity: ${data.main.humidity}`;
+        const imageURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        fetch(imageURL) 
+        .then((response) => {
+        return response;
+    })
+    .then((data) => {
+        document.querySelector('#current-icon').src = data.url;
+    })
+    getFiveDayURL(data.coord.lat, data.coord.lon);
+});
+}
+function getFiveDayURL(latitude, longitude) {
+console.log(latitude, longitude);
+const fiveDayURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=imperial`;
+console.log(fiveDayURL);
+fetch(fiveDayURL)
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+}
