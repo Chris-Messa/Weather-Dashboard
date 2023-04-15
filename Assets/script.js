d9d6fdb2f744db04d172794daedea9338cc5c127
Chris Messa <blindsun95@gmail.com>
2023-04-14 23:01:41 -0500

    Implemented 5 day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -13,52 +13,55 @@ cityContainer.setAttribute('style', 'display: none;')
 searchButton.addEventListener('click', function () {
     cityContainer.setAttribute('style', 'display: block')
     const searchedCity = searchInputEl.value.toLowerCase().trim();
-    
+
     queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${APIKey}&units=imperial`;
 
     getCoords();
 });
 function getCoords() {
     fetch(queryURL)
-    .then((response) => {
-        return response.json();
-    })
-    .then(function (data) {
-        cityNameEl.textContent = `${data.name}`;
-        cityTempEl.textContent = `Temp: ${data.main.temp}`;
-        cityWindEl.textContent = `Wind: ${data.wind.speed} MPH`;
-        cityHumidityEl.textContent = `Humidity: ${data.main.humidity}`;
-        const imageURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-       
-        getIcon(imageURL)
-        getFiveDayURL(data.coord.lat, data.coord.lon);
-});
+        .then((response) => {
+            return response.json();
+        })
+        .then(function (data) {
+            cityNameEl.textContent = `${data.name}`;
+            cityTempEl.textContent = `Temp: ${data.main.temp} °F`;
+            cityWindEl.textContent = `Wind: ${data.wind.speed} MPH`;
+            cityHumidityEl.textContent = `Humidity: ${data.main.humidity}%`;
+            const imageURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+            getIcon(imageURL)
+            getFiveDayURL(data.coord.lat, data.coord.lon);
+        });
 }
 
 function getIcon(link) {
     fetch(link)
-    .then((response) => {
-        return response;
-    })
-    .then((data) => {
-        document.querySelector('#current-icon').src = data.url;
-    })
+        .then((response) => {
+            return response;
+        })
+        .then((data) => {
+            document.querySelector('#current-icon').src = data.url;
+        })
 }
 function getFiveDayURL(latitude, longitude) {
-const fiveDayURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=imperial`;
-console.log(fiveDayURL);
-fetch(fiveDayURL)
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-    var listAt = 4;
-for (let i = 0; i < forecastElList.children.length; i++){
-    let forecastChildren = forecastElList.children[i].children[0];
-    forecastChildren.innerHTML = `<div id="temp-${i}">${data.list[listAt].main.temp}</div>`
-    forecastChildren.children[i]
-    listAt += 8;
-}
-})
+    const fiveDayURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=imperial`;
+    console.log(fiveDayURL);
+    fetch(fiveDayURL)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            var listAt = 2;
+            for (let i = 0; i < forecastElList.children.length; i++) {
+                let forecastChildren = forecastElList.children[i];
+                forecastChildren.children[0].children[0].textContent = data.list[listAt].dt_txt.slice(0, 10);
+                forecastChildren.children[0].children[1].textContent = `Temp: ${data.list[listAt].main.temp} °F`;
+                forecastChildren.children[0].children[2].textContent = `Wind: ${data.list[listAt].wind.speed} MPH`;
+                forecastChildren.children[0].children[3].textContent = `Humidity: ${data.list[listAt].main.humidity}%`;
+                
+                listAt += 8;
+            }
+        })
 }
